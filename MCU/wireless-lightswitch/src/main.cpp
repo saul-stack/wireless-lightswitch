5e44621f424637048eab1520ae5a10ad9af04ccd
Saul Smith <125879463+saul-stack@users.noreply.github.com>
2024-10-02 15:09:59 +0100

    feat: define functions for toggling built in LED defined functions "activateLED" and "deactivateLED" to toggle the built in LED on the ESP8266WiFi board.

--- MCU/wireless-lightswitch/src/main.cpp
+++ MCU/wireless-lightswitch/src/main.cpp
@@ -3,3 +3,9 @@
 #include <ESP8266WebServer.h>
 const char* ssid = WIFI_SSID;
 const char* password = PASSWORD;
+void activateLED() {
+  digitalWrite(LED_BUILTIN, LOW);
+}
+void deactivateLED() {
+  digitalWrite(LED_BUILTIN, HIGH);
+}
