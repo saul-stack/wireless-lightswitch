7b4b314e32c277a10ac8fa0bfc24a081fe9b9012
Saul Smith <125879463+saul-stack@users.noreply.github.com>
2024-10-06 16:27:40 +0100

    feat: update HomePage with new request buttons
    Added buttons to the HomePage which send http
    requests to the MCU in the backend.

--- web-app/frontend/src/Components/HomePage.jsx
+++ web-app/frontend/src/Components/HomePage.jsx
@@ -1,3 +1,42 @@
+import { requestLEDAction } from "../../api-utils";
 export const HomePage = () => {
-  return <h1>Homepage</h1>;
+  const handleButtonClick = (action) => {
+    requestLEDAction({ action: action });
+  };
+  return (
+    <div className="main-content">
+      <div className="button-container">
+        <h1>Wireless Lightswitch</h1>
+        <div className="button-container">
+          <button
+            onClick={() => handleButtonClick("activate")}
+            className="button"
+          >
+            Turn On
+          </button>
+          <button
+            onClick={() => handleButtonClick("deactivate")}
+            className="button"
+          >
+            Turn Off
+          </button>
+          <button onClick={() => handleButtonClick("blink")} className="button">
+            Blink
+          </button>
+          <button
+            onClick={() => handleButtonClick("toggle")}
+            className="button"
+          >
+            Toggle
+          </button>
+          <button
+            onClick={() => handleButtonClick("flashing")}
+            className="button"
+          >
+            Flashing
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
