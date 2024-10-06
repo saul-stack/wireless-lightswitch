db3465e59dc86a52afe8fe0aa24e6d318dfcb245
Saul Smith <125879463+saul-stack@users.noreply.github.com>
2024-10-06 18:48:00 +0100

    config: update dotenv path in vite.config.js Updated path to .env files in vite.config.js. It will now look for .env files in the root directory of the entire project. This means that the frontend and backend will share the same .env directory in the project's global root directory.

--- web-app/frontend/vite.config.js
+++ web-app/frontend/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import dotenv from "dotenv";
+import path from "path";
+import react from "@vitejs/plugin-react";
+
+dotenv.config({ path: path.resolve(__dirname, "../../.env.development") });
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+});
