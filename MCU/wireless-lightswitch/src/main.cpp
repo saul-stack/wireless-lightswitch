f948220a7c55a8ecdaec4596d10e1962aa6b2d5b
Saul Smith <125879463+saul-stack@users.noreply.github.com>
2024-10-07 16:15:59 +0100

    feat: configure WiFiManager library for MCU Implemented the WifiManager library so that users can configure the device's WiFi settings without re-flashing the firmware, using a smartphone or other computer.

--- MCU/wireless-lightswitch/src/main.cpp
+++ MCU/wireless-lightswitch/src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <ArduinoJson.h>
 #include <ESP8266WebServer.h>
+#include <WiFiManager.h>
 
 #include "utils/led_functions.h"
 #include "utils/connection_functions.h"
@@ -9,12 +10,22 @@
 const int PORT = 80;
 
 ESP8266WebServer server(PORT);
+WiFiManager wifiManager;
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   deactivateLed();
 
   Serial.begin(9600);
+
+  if (!wifiManager.autoConnect(BOARD_HOSTNAME)) {
+    Serial.println("Failed to connect and hit timeout");
+    ESP.reset();
+  }
+
+  Serial.println("Connected to Wi-Fi: " + String(WiFi.SSID()));
+  WiFi.hostname(BOARD_HOSTNAME);
+  Serial.println("Board Hostname: " + String(BOARD_HOSTNAME));
 
   startServer();
 
